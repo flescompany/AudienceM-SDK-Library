ca8d80571d06b0cafe6f3c83d5874617f11d8932
KimYongYeon <duszld604@fles.co.kr>
2021-06-14 12:32:55 +0900

    feat : init

---
+++ AudienceM-SDK-Library/src/main/java/com/oneclick/audiencem_sdk_library/ToastClass.kt
@@ -0,0 +1,13 @@
+package com.oneclick.audiencem_sdk_library
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastClass {
+
+    companion object {
+
+        fun showToast(context: Context, message: String) = Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
