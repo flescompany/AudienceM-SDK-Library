ae42fa4d9e4f927e36dd91a4a000800f4d113f3e
KimYongYeon <duszld604@fles.co.kr>
2021-06-17 17:17:14 +0900

    feat : segment 연동 라이브러리

---
+++ AudienceM-SDK-Library/src/main/java/com/oneclick/audiencem_sdk_library/AudienceMSDK.kt
@@ -0,0 +1,61 @@
+package com.oneclick.audiencem_sdk_library
+
+import android.content.Context
+import android.os.AsyncTask
+import android.util.Log
+import com.google.gson.*
+import com.segment.analytics.Analytics
+import com.segment.analytics.Properties
+import org.json.JSONObject
+
+
+class AudienceMSDK(var mContext: Context, var AMKey: String) : AsyncTask<Void?, Void?, String?>() {
+
+    fun AMInit(AMName:String, AMData: JSONObject) {
+        val propertiesData = toProperties(AMData)
+
+        val analytics = Analytics.Builder(mContext, AMKey)
+                .trackApplicationLifecycleEvents() // Enable this to record certain application events automatically!
+                .recordScreenViews() // Enable this to record screen views automatically!
+                .build()
+        Analytics.setSingletonInstance(analytics);
+
+        Analytics.with(mContext).track(AMName, propertiesData)
+    }
+
+    fun AMTrack(AMName:String, AMData: JSONObject) {
+        val propertiesData = toProperties(AMData)
+
+        Analytics.with(mContext).track(AMName, propertiesData)
+    }
+
+
+    fun toProperties(JsonData: JSONObject) : Properties {
+        val propertiesData = Properties()
+
+        val parser = JsonParser()
+        val element: JsonElement = parser.parse(JsonData.toString())
+        val obj: JsonObject = element.getAsJsonObject() //since you know it's a JsonObject
+        val entries: Set<Map.Entry<String?, JsonElement?>> = obj.entrySet() //will return members of your object
+
+        for ((key, value) in entries) {
+            val stringCheck: Int = java.lang.String.valueOf(value).indexOf("\"")
+            if (stringCheck == 0) {
+                var propertiesDataValue: String = java.lang.String.valueOf(value)
+                propertiesDataValue = propertiesDataValue.replaceFirst("\"".toRegex(), "")
+                propertiesDataValue = propertiesDataValue.substring(0, propertiesDataValue.lastIndexOf("\""))
+                propertiesData.putValue(key, propertiesDataValue)
+            } else {
+                propertiesData.putValue(key, value)
+            }
+        }
+
+        return propertiesData
+    }
+
+    override fun doInBackground(vararg params: Void?): String? {
+        TODO("Not yet implemented")
+    }
+}
+
+
--- app/src/main/java/com/oneclick/testandroidlib/MainActivity.java
+++ app/src/main/java/com/oneclick/testandroidlib/MainActivity.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.oneclick.audiencem_sdk_library.ToastClass;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ToastClass.Companion.showToast(this,"하이루 뭐야 왜 안떠");
-
         setContentView(R.layout.activity_main);
     }
 }
